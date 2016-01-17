845041739dac4308a9b0c2a76b85fc5dd65a5e7d
Joakim Jalap <joakim.jalap@gmail.com>
2016-01-17 11:42:24 +0100

    Begin working on code

---
+++ src/jjfs_sftp.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2016  Joakim Jalap
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "libssh/sftp.h"
+#include "jjfs_sftp.h"
+
+const char * jjfs_server;
+
+int jjfs_port;
+
+ssh_session ssh;
+sftp_session sftp;
+
+int jjfs_conn() {
+  
+}
+
+int jjfs_disconn() {
+
+}
